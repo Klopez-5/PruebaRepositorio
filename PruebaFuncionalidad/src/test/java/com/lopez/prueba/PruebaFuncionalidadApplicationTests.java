041a57cfc3734cc56b03d29594a1d54dbc4e0787
Klopez-5 <kalopez5@espe.edu.ec>
2020-06-16 17:42:05 -0500

    estoy probando que funcione

---
+++ PruebaFuncionalidad/src/test/java/com/lopez/prueba/PruebaFuncionalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopez.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFuncionalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
